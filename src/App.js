5a61b0db76a959b50e47dff2d125174263bc9385
Milen <milenmihalkov@gmail.com>
2023-02-02 16:20:25 +0200

    localStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [text, setText] = useState(() => {
+    return localStorage.getItem("note") || "";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("note", text);
+  }, [text]);
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea 
+            className="textarea is-large" 
+            placeholder="Notes..." 
+            value={text} 
+            onChange={(e) => setText(e.target.value)}
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button 
+        className="button is-large is-primary is-active" 
+        onClick={() => localStorage.setItem("note", text)}>
+          Save</button>
+        <button 
+        className="button is-large" 
+        onClick={() => setText("")}>
+          Clear</button>
       </div>
     </div>
   );
